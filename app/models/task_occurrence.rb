3283cb91d0330bedd781248eef1c05b3dbec22e7
jcalca1 <jeffcalcagno@gmail.com>
2015-08-26 17:41:29 -0500

    sign in

--- app/models/task_occurrence.rb
+++ app/models/task_occurrence.rb
@@ -2,4 +2,5 @@ class TaskOccurrence < ActiveRecord::Base
    validates :task_id, :presence => true
 
   belongs_to :tasks
+  belongs_to :users
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
  has_many :tasks
+ has_many :task_occurrences, :through =>:tasks
 
 end
