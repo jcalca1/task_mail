0862eea17556ffc463e33e82004f82128dd8bf70
jcalca1 <jeffcalcagno@gmail.com>
2015-08-28 00:13:39 -0500

    update

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -79,7 +79,7 @@ def update
   @task.recurrence_frequency_num = params[:recurrence_frequency_num]
   @task.recurrence = params[:recurrence]
   @task.notes = params[:notes]
-  @task.date = params[:date]
+  @task.date = Chronic.parse(params[:date])
   @task.name = params[:name]
   if @task.save
    @task.task_occurrences.each do |task_occurrence|
